3e9fe56837b5663e2af174f844e3eb68370117a4
Amir <Amir@10.0.0.180>
2019-09-12 00:14:15 -0700

    My First attempt to simulate Max calculator APP

--- index.php
+++ index.php
@@ -1,55 +1,56 @@
-
 <!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
 <html>
     <head>
-         
-        <title>Calculator</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-     
+        <meta charset="UTF-8">
+        <title></title>
+        <link href="mystyle.css" rel="stylesheet">
+        
     </head>
     <body>
-            <p style="font-size:100px; margin-bottom: 8px;">&#129409;</p>
-          
-            <div class= "grid-container">
-                
-                         <div id="display-result" class="display-digit" > 0 </div>
 
-                         <div id="op-clean" class="key-digi decor2" >AC</div>
-                         <div id="op-minus" class="key-op decor2" >+/-</div>
-                         <div id="op-percentage" class="key-op decor2">%</div>
-                         
-                         <div id="op-division" class="key-op decor1">÷</div>
+        <!--<header> This is a simulation of Mac Calculator.. </header>-->
+        
+        <section class="calc-section">      
+            <!--<p> Mac Calculator</p>-->
+        
+         
+         <div class= "grid-container">
+            
+                         <div id="display-result" class="display-digit" > 0 </div>       
 
-                         <div id="op-num-7" class="key-digi item5">7</div>
-                         <div id="op-num-8" class="key-digi item6">8</div>
-                         <div id="op-num-9" class="key-digi item7">9</div>
-                         
-                         <div id="op-multiply" class="key-op decor1" >×</div>
+                         <div id="op-clean" class="key-digi key-prop2" >AC</div>
+                         <div id="op-minus" class="key-digi key-prop2" >+/-</div>
+                         <div id="op-percentage" class="key-digi key-prop2">%</div>
+                         <div id="op-division" class="key-op key-prop1">&#247;</div>
 
-                         <div id="op-num-4" class="key-digi item9">4</div>
-                         <div id="op-num-5" class="key-digi item10">5</div>
-                         <div id="op-num-6" class="key-digi item11">6</div>
-                         
-                         <div id="op-minus" class="key-op decor1" >-</div>
+                         <div id="op-num-7" class="key-digi ">7</div>
+                         <div id="op-num-8" class="key-digi">8</div>
+                         <div id="op-num-9" class="key-digi">9</div>
+                         <div id="op-num-multiply" class="key-op key-prop1" >&#215</div>
 
-                         <div id="op-num-1" class="key-digi item13">1</div>
-                         <div id="op-num-2" class="key-digi item14">2</div>
-                         <div id="op-num-3" class="key-digi item15">3</div>
-                         
-                         <div id="op-add" class="key-op decor1">+</div>
+                         <div id="op-num-4" class="key-digi">4</div>
+                         <div id="op-num-5" class="key-digi">5</div>
+                         <div id="op-num-6" class="key-digi">6</div>
+                         <div id="op-num-minus" class="key-op key-prop1" >-</div>
+
+                         <div id="op-num-1" class="key-digi">1</div>
+                         <div id="op-num-2" class="key-digi">2</div>
+                         <div id="op-num-3" class="key-digi">3</div>
+                         <div id="op-num-add" class="key-op key-prop1">+</div>
 
                          <div id="op-num-0" class="key-digi double-size">0</div>
-                         <div id="op-float" class="key-digi item17">.</div> 
-                         <div id="op-equal" class="key-op decor1">&#61;</div>
-         </div>      
-
-            <br><br>
-            <h1  style="color: red; margin-left: 452px" id="prt"> Print the output here...</h1>   
-         
-
-
+                         <div id="op-num-float" class="key-decimal item17">.</div> 
+                         <div id="op-num-equal" class="key-op key-prop1">=</div>
+            
+            
+        </div>
+     </section>
+     <script type = "text/javascript" src = "calc.js" ></script>  
 
     </body>
-
 </html>
